5cd46eecf35ca4036a1683555e1c465cc75fd101
Sergey Kononenko <hackfeed@yandex.ru>
2021-05-11 01:40:57 +0300

    Add action for uploading data to backend

--- frontend/src/store/modules/analytics/actions.js
+++ frontend/src/store/modules/analytics/actions.js
@@ -1,19 +1,17 @@
 export default {
-  async uploadData(context) {
-    // const file = data.file;
+  async uploadData(context, data) {
+    const response = await fetch("http://localhost:8081/upload", {
+      method: "POST",
+      body: data,
+    });
+    const responseData = await response.json();
 
-    // const response = await fetch("path/to/backend", {
-    //   method: "POST",
-    //   body: file,
-    // });
-    // const responseData = await response.json();
+    if (!response.ok) {
+      const error = new Error(responseData.message || "Failed to fill data.");
+      throw error;
+    }
 
-    // if (!response.ok) {
-    //   const error = new Error(responseData.message || "Failed to fill data.");
-    //   throw error;
-    // }
-
-    context.commit("setDataIsUploaded", true);
+    context.commit("setDataIsUploaded", responseData.isUploaded);
   },
   async loadData(context) {
     // const periodStart = data.periodStart;
